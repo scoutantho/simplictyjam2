45b94cda370a3e201502a572155b1b51efca5163
anthony <anthony.gregori78200@gmail.com>
2024-12-03 21:13:25 -0500

    Add Godot 4 editor path, update scene structures, and implement game logic for tile placement

---
+++ Scripts/game.gd
@@ -0,0 +1,43 @@
+extends Node3D
+
+const dir = [Vector2.RIGHT, Vector2.LEFT, Vector2.UP, Vector2.DOWN]
+
+var grid_size = 10
+var grid_steps = 5 #number of tiles ?
+
+var tilesAlreadyPresent = [Vector2(0,0),Vector2(-1,0), Vector2(0,-1), Vector2(-1,-1) ]
+
+func _ready():
+	setDefaultSpawn()
+	randomize()
+	var current_pos = Vector2(0,0)
+	
+	var current_dir = Vector2.RIGHT
+	var last_dir = current_dir * -1
+	
+	for i in range(grid_steps):
+		var temp_dir = dir.duplicate()
+		temp_dir.shuffle()
+		var d = temp_dir.pop_front()
+
+		while temp_dir.size() > 0:  # Ensure we don't exhaust all directions without valid movement
+			var new_pos = current_pos + d
+			
+		# Check grid boundaries and duplicate tiles
+			if abs(new_pos.x) <= grid_size and abs(new_pos.y) <= grid_size and new_pos not in tilesAlreadyPresent:
+				break  # Valid direction found
+				
+			# Try another direction
+			d = temp_dir.pop_front()
+			
+		# Check if a valid direction was found
+		var new_pos = current_pos + d
+		if abs(new_pos.x) <= grid_size and abs(new_pos.y) <= grid_size and new_pos not in tilesAlreadyPresent:
+			current_pos = new_pos
+			tilesAlreadyPresent.append(current_pos)
+			$GridMap.set_cell_item(Vector3i(int(current_pos.x), 0, int(current_pos.y)), 0, 0)
+			$GridMap.set_cell_item(Vector3i(int(current_pos.x), 5, int(current_pos.y)), 0, 0)
+
+func setDefaultSpawn():
+	for v in tilesAlreadyPresent:
+		$GridMap.set_cell_item(Vector3i(int(v.x), 0, int(v.y)), 0, 0)
