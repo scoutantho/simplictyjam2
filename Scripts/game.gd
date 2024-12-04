1eb2f5799f917e644a998e673dd905c99e84bbfa
anthony <anthony.gregori78200@gmail.com>
2024-12-03 21:32:35 -0500

    Enhance game mechanics by adding wall generation, updating player and camera configurations, and refining tile properties

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -2,8 +2,9 @@ extends Node3D
 
 const dir = [Vector2.RIGHT, Vector2.LEFT, Vector2.UP, Vector2.DOWN]
 
-var grid_size = 10
-var grid_steps = 5 #number of tiles ?
+var grid_size = 100
+var grid_steps = 500 #number of tiles ?
+var wall_height = 3
 
 var tilesAlreadyPresent = [Vector2(0,0),Vector2(-1,0), Vector2(0,-1), Vector2(-1,-1) ]
 
@@ -36,8 +37,19 @@ func _ready():
 			current_pos = new_pos
 			tilesAlreadyPresent.append(current_pos)
 			$GridMap.set_cell_item(Vector3i(int(current_pos.x), 0, int(current_pos.y)), 0, 0)
-			$GridMap.set_cell_item(Vector3i(int(current_pos.x), 5, int(current_pos.y)), 0, 0)
+			# $GridMap.set_cell_item(Vector3i(int(current_pos.x), 5, int(current_pos.y)), 0, 0)
+	add_walls()
 
 func setDefaultSpawn():
 	for v in tilesAlreadyPresent:
 		$GridMap.set_cell_item(Vector3i(int(v.x), 0, int(v.y)), 0, 0)
+
+func add_walls():
+	var neighbor_offsets = [Vector2(1, 0), Vector2(-1, 0), Vector2(0, 1), Vector2(0, -1)]
+
+	for tile in tilesAlreadyPresent:
+		for offset in neighbor_offsets:
+			var neighbor_pos = tile + offset
+			if neighbor_pos not in tilesAlreadyPresent:  # If the position is empty
+				for height in range(wall_height):  # Build a 3-tall wall
+					$GridMap.set_cell_item(Vector3i(int(neighbor_pos.x), height, int(neighbor_pos.y)), 1, 0)  # Replace '1' with wall ID
