d3a894b1b20bae84c6896f9fa06d3324e265ad24
anthony <anthony.gregori78200@gmail.com>
2024-12-04 20:04:28 -0500

    Increase grid size to enhance tile placement and game mechanics

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -2,7 +2,7 @@ extends Node3D
 
 const dir = [Vector2.RIGHT, Vector2.LEFT, Vector2.UP, Vector2.DOWN]
 
-var grid_size = 100
+var grid_size = 1000
 var grid_steps = 500 #number of tiles ?
 var wall_height = 3
 var _isRoofEnable: bool = true
