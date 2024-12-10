6b020de89a6e621d6650e0ad445894176d7d6cbb
anthony <anthony.gregori78200@gmail.com>
2024-12-09 22:14:05 -0500

    Add magic transition sound effect and implement game scene swapping logic

--- gameManager/game_manager.gd
+++ gameManager/game_manager.gd
@@ -6,3 +6,33 @@ var numberOfLevels := 3
 var actualLevel := 1
 
 @onready var gameTheme = $"/root/GameManager/gametheme"
+@onready var gameSwap = $"/root/GameManager/gameSwap"
+@onready var gameSwapTimer = $"/root/GameManager/gameSwapTimer" 
+
+func newGame():
+	GameManager.actualLevel += 1
+	GameManager.actualGridsize += 2000
+	GameManager.actualGridSteps += 50
+
+	# Load the new scene
+	var new_scene = load("res://Scenes/game.tscn") as PackedScene
+	var instance = new_scene.instantiate()
+
+	# Properly replace the current scene
+	var current_scene = get_tree().root.get_child(1)  # Assume the current scene is the first child
+	get_tree().root.add_child(instance)  # Add the new scene
+	get_tree().current_scene = instance
+	current_scene.queue_free()
+
+
+func handleGameTheme():
+	if GameManager.gameTheme != null and  (GameManager.gameTheme as AudioStreamPlayer).playing == false:
+		(GameManager.gameTheme as AudioStreamPlayer).play()
+
+func GameSwap():
+	GameManager.gameSwap.play()
+	GameManager.gameSwapTimer.start()
+
+func _on_game_swap_timer_timeout() -> void:
+	GameManager.newGame()
+	pass # Replace with function body.
